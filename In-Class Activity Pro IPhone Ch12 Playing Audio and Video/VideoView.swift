421df7010025df8b326d74295fa735932c6cd222
Tori Lew <torirlew@gmail.com>
2023-11-28 18:51:16 -0800

    There is a video

--- In-Class Activity Pro IPhone Ch12 Playing Audio and Video/VideoView.swift
+++ In-Class Activity Pro IPhone Ch12 Playing Audio and Video/VideoView.swift
@@ -4,12 +4,32 @@
 //
 //  Created by Student Account on 11/28/23.
 //
+// Video source:  https://www.pexels.com/video/close-up-of-a-black-capped-chickadee-on-a-person-s-hand-9803926/
 
 import SwiftUI
+import AVKit
 
 struct VideoView: View {
+    @State var player: AVPlayer?
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VideoPlayer(player: player, videoOverlay: {
+            VStack{
+                Spacer()
+                Text("Here is a chickadee")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .background(.black)
+                Spacer()
+                Spacer()
+            }
+        })
+            .onAppear {
+                guard let videoURL = Bundle.main.url(forResource: "nature-walk", withExtension: "mp4") else {
+                    print ("Video file not found")
+                    return
+                }
+                player = AVPlayer(url: videoURL as URL)
+            }
     }
 }
 
