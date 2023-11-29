9cb9f224f246dbe112990a0131c7bef65a4e56dc
Tori Lew <55900873+Tori-Lew@users.noreply.github.com>
2023-11-28 17:24:47 -0800

    Initial commit

---
+++ In-Class Activity Pro IPhone Ch12 Playing Audio and Video/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  In-Class Activity Pro IPhone Ch12 Playing Audio and Video
+//
+//  Created by Student Account on 11/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ In-Class Activity Pro IPhone Ch12 Playing Audio and Video/In_Class_Activity_Pro_IPhone_Ch12_Playing_Audio_and_VideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  In_Class_Activity_Pro_IPhone_Ch12_Playing_Audio_and_VideoApp.swift
+//  In-Class Activity Pro IPhone Ch12 Playing Audio and Video
+//
+//  Created by Student Account on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct In_Class_Activity_Pro_IPhone_Ch12_Playing_Audio_and_VideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
