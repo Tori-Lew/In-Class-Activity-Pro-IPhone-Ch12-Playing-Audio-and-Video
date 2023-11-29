bd6b5b302d4bcde21edd074928239cd0ae154dfc
Tori Lew <torirlew@gmail.com>
2023-11-28 18:17:53 -0800

    Audio is added

---
+++ In-Class Activity Pro IPhone Ch12 Playing Audio and Video/AudioView.swift
@@ -0,0 +1,29 @@
+//
+//  AudioView.swift
+//  In-Class Activity Pro IPhone Ch12 Playing Audio and Video
+//
+//  Created by Student Account on 11/28/23.
+//
+// Audio source: I have had this audio in my files for so long I have forgotten
+// where it came from originally. I think it was a YouTube video
+import SwiftUI
+
+struct AudioView: View {
+    @State var playAudio = true
+    var body: some View {
+        Button(action: {
+            if playAudio {
+                playSound(sound: "Chickadee", type: "mp3")
+            } else {
+                stopSound()
+            }
+            playAudio.toggle()
+        }, label: {
+            Text(playAudio ? "Start the bird" : "Stop the bird")
+        })
+    }
+}
+
+#Preview {
+    AudioView()
+}
--- In-Class Activity Pro IPhone Ch12 Playing Audio and Video/ContentView.swift
+++ In-Class Activity Pro IPhone Ch12 Playing Audio and Video/ContentView.swift
@@ -7,15 +7,36 @@
 
 import SwiftUI
 
+import AVFoundation
+import AVKit
+
+var audioPlayer: AVAudioPlayer?
+func playSound(sound: String, type: String) {
+    if let path = Bundle.main.path(forResource: sound, ofType: type) {
+        do {
+            audioPlayer = try AVAudioPlayer(
+                contentsOf: URL(fileURLWithPath: path))
+            audioPlayer?.play()
+        } catch {
+            print("Could not find and play the sound file")
+        }
+    }
+}
+func stopSound() {
+    audioPlayer?.stop()
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView(){
+            AudioView()
+                .tabItem{Label("Audio", systemImage: "speaker.wave.2.fill")}
+                .toolbarBackground(.visible, for: .tabBar)
+            
+            VideoView()
+                .tabItem{Label("Video", systemImage: "video.fill")}
+                .toolbarBackground(.visible, for: .tabBar)
         }
-        .padding()
     }
 }
 
